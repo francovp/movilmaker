fa3fc3957d6f1547603b18048dc46a43e3db00cc
Franco <franco.gvp@hotmail.com>
2015-10-16 13:22:09 -0400

    Actualizado Principal para eliminar cosas innecesarias

--- src/Principal.java
+++ src/Principal.java
@@ -23,9 +23,9 @@ public class Principal {
 		//Conexion a la base de datos de Postgres
 		Database bd = new Database();
 
-		Compania datosEmpresa = null; // Aqui­ se guardaran los datos de la clase empresa
+		Compania datosEmpresa = null; // AquiÂ­ se guardaran los datos de la clase empresa
 
-		// Conexión la base de datos de Postgres
+		// ConexiÃ³n la base de datos de Postgres
 		datosEmpresa = bd.leerDatosBD();
 		if (datosEmpresa != null)
 			// Si se crearon los datos de la empresa
@@ -39,18 +39,5 @@ public class Principal {
 //================================Llama a FrameInterfaz==========================================
 		FrameInterfaz iPrincipal = new FrameInterfaz(datosEmpresa);
 		iPrincipal.setVisible(true);
-
-		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-		String wait;
-		wait= bf.readLine();
-		System.out.println("hola");
-
-		for(int i=0;i<datosEmpresa.getListaClientes().size();i++){
-		System.out.println("\nMuestra en principal!\nNombre1 "+datosEmpresa.getListaClientes().get(i).getNombre1()+
-				"\nNombre2 "+datosEmpresa.getListaClientes().get(i).getNombre2()+
-				"\nrut "+datosEmpresa.getListaClientes().get(i).getRut()+
-				"\nnumero movil "+datosEmpresa.getListaClientes().get(i).getFonoCel());
-		System.out.println("Plan: "+datosEmpresa.getListaClientes().get(i).getContratos().get(0).getPlanContratado().getNombrePlan()+"Cuota: "+datosEmpresa.getListaClientes().get(i).getContratos().get(0).getCuotas());
-		}
 	}
 }
